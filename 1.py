9880b1766360c8e2db02d795c1cae93e158ba147
Jazpy <jaz.medtre@gmail.com>
2021-11-30 23:36:28 -0600

    Day 1, finally December!

---
+++ 1.py
@@ -0,0 +1,15 @@
+with open('inputs/1.txt', 'r') as in_f:
+  reads = in_f.readlines()
+
+reads = [int(read) for read in reads]
+
+past = reads[0]
+count = 0
+
+for read in reads[1:]:
+  if read > past:
+    count += 1
+
+  past = read
+
+print(count)
---
+++ 2.py
@@ -0,0 +1,17 @@
+with open('inputs/1.txt', 'r') as in_f:
+  reads = in_f.readlines()
+
+reads = [int(read) for read in reads]
+
+past = reads[0] + reads[1] + reads[2]
+count = 0
+
+for i in range(1, len(reads) - 2):
+  curr_window = past + reads[i + 2] - reads[i - 1]
+
+  if curr_window > past:
+    count += 1
+
+  past = curr_window
+
+print(count)
