6bcc89d3e0a49ecc20cceb055fd415b89b6c9a5d
Jazeps Medina Tretmanis <jmedinat@node1101.oscar.ccv.brown.edu>
2021-12-06 12:02:54 -0500

    days 4 through 6

---
+++ 12.py
@@ -0,0 +1,18 @@
+lanternfish = [0] * 9
+
+with open('inputs/11.txt', 'r') as in_f:
+    start = [int(x) for x in in_f.readline().split(',')]
+
+for s in start:
+    lanternfish[s] += 1
+
+iters = 256
+
+for _ in range(iters):
+    spawners = lanternfish[0]
+    for j in range(len(lanternfish) - 1):
+        lanternfish[j] = lanternfish[j + 1]
+    lanternfish[6] += spawners
+    lanternfish[8]  = spawners
+
+print(sum(lanternfish))
---
+++ 8.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+boards = []
+with open('inputs/7.txt', 'r') as in_f:
+    numbers = [int(x) for x in in_f.readline().split(',')]
+    in_f.readline()
+
+    curr_board = []
+    for line in in_f.readlines():
+        if line.strip() == '':
+            boards.append(np.array(curr_board))
+            curr_board = []
+            continue
+
+        curr_board.append(np.array([int(x) for x in line.split()]))
+
+def verify_board(b, n):
+    b[b == n] = -1
+
+    for i in range(5):
+        if sum(b[:, i]) == -5 or sum(b[i, :]) == -5:
+            return True
+
+    return False
+
+def get_score(b, n):
+    return sum(b[b != -1]) * n
+
+last_score = 0
+for n in numbers:
+    for idx, b in enumerate(boards):
+        if b.any() and verify_board(b, n):
+            last_score = get_score(b, n)
+            boards[idx] = np.empty(0)
+
+print(last_score)
